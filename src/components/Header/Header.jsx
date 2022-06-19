baf9732755121b7cf6ea5e9274eea3b06b3c9d1b
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2022-06-20 00:55:52 +0600

    login form design

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <Navbar bg="light" expand="lg" className='bg-white shadow-sm'>
+            <Container>
+                <Navbar.Brand href="#" className='navbar-brand fs-3 fw-bold'>
+                    ATools<span className='dot'>.</span>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll" >
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+
+                        <Button className='mx-2 my-1 border-0 rounded-1 btn-trial'>Start Free Trial</Button>
+                        <Button className='mx-2 my-1 border-0 rounded-1 btn-login'>Login</Button>
+                    </Nav>
+
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar >
+    );
+};
+
+export default Header;
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { Form, Button, Container, Row, Col, Alert } from 'react-bootstrap';
+import './Login.css';
+import loginImage from '../../images/login-image.png';
+
+const Login = () => {
+    const [loginData, setLoginData] = useState({});
+    console.log(loginData);
+    const [message, setMessage] = useState('');
+    const [show, setShow] = useState(false);
+
+    const handleOnBlur = (e) => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLogindata = { ...loginData };
+        newLogindata[field] = value;
+        setLoginData(newLogindata);
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        fetch("https://reqres.in/api/login", {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(loginData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setMessage(data)
+                e.target.reset();
+
+            })
+
+    }
+    return (
+        <div>
+            <Container>
+                <Row className='mx-auto d-flex justify-content-space-between align-items-center' xs={1} md={2} lg={2}>
+                    <Col xs={12} md={6} lg={4}>
+                        <Form className='mx-auto my-3 px-3' onSubmit={handleSubmit}>
+                            <div className='text-center my-3'>
+                                <h2 className='title'>Welcome Back</h2>
+                                <p>Please Login here</p>
+                            </div>
+                            {
+                                message && message.error && (
+                                    <Alert
+                                        variant="danger" onClose={() => setShow(false)} dismissible
+                                    >{message?.error}</Alert>
+                                )
+                            }
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Control onBlur={handleOnBlur} type="email" name="email" placeholder="Email Address *" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Control onBlur={handleOnBlur} type="password" name="password" placeholder="Password *" />
+                            </Form.Group>
+
+                            <Button className='btn-trial w-100 mb-3' variant="primary" type="submit">
+                                Login
+                            </Button>
+                            <div className='remember-forget'>
+                                <Form.Group className="mb-1" controlId="formBasicCheckbox">
+                                    <Form.Check type="checkbox" label="Remember Password" />
+                                </Form.Group>
+                                <p className=''>Forget password</p>
+                            </div>
+                        </Form>
+                    </Col>
+                    <Col xs={12} md={6} lg={8}>
+                        <img className='mx-auto img-fluid  px-3' src={loginImage} alt="" />
+                    </Col>
+                </Row>
+            </Container>
+        </div >
+    );
+};
+
+export default Login;
